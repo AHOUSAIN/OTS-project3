a297b730bada6c7746f93d0e008f3d6a8812dc01
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-24 14:49:33 +0300

    removed user

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,12 @@ ActiveRecord::Schema.define(:version => 20121123224039) do
     t.string   "account_number"
     t.string   "account_name"
     t.string   "email"
-    t.string   "password_hash"
     t.string   "password_salt"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.boolean  "admin"
+    t.string   "password_hash"
   end
 
 end
