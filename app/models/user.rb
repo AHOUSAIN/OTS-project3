2b5b512f50790048b046b49d9966087a6e98a5a5
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-26 08:29:17 +0300

    added controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :account_name, :account_number, :email, :mobile_number, :name, :password_digest
+  has_secure_password
+  
+  validates_presence_of :password, :on => :create
 end
