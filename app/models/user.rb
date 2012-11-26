f78003aa6a1c053f4da237ba8c70f7870707e0c0
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-26 08:58:00 +0300

    Completed Sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :account_name, :account_number, :email, :mobile_number, :name, :password_digest
+  attr_accessible :account_name, :account_number, :email, :mobile_number, :name, :password,:password_confirmation ,:password_digest
   has_secure_password
   
   validates_presence_of :password, :on => :create
