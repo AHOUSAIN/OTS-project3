b6d2721494417bdf3995065e4fe6b9bcb721c774
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-26 15:32:04 +0300

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 OTS::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
     get "log_in" => "sessions#new", :as => "log_in"
     get "log_out" => "sessions#destroy", :as => "log_out"
 
