7c171bd1a2e10c11eae57585262bce4f3834bffb
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-28 04:16:14 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OTS::Application.config.secret_token = '3b18c1414f680f7833c7334c02d6782fd260bb7c9ba34131d27d0ec7f7bae32aa4b3ee4441017090d1f4a71ed288aee4799fc66590a17523e043b5f1e5163d46'
---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  
+  factory :user do
+    
+    name "Afrah"
+    email "foobar@foobar.com"
+    mobile_number "97455642248"
+    account_name  "Afrah"
+    account_number "444-444-2005"
+    password "foobar"
+    password_confirmation "foobar"
+    
+end
+end
